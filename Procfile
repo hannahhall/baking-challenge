b4a169ea35b272069cfd46242c341a5ab8cefe2f
hannahhall <hannahkhall13@gmail.com>
2022-01-07 17:32:38 -0600

    set up deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn baking_project.wsgi --log-file -
--- baking_project/settings.py
+++ baking_project/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,6 +126,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -137,3 +139,5 @@ cloudinary.config(
     api_key=os.environ.get('CLOUDINARY_API_KEY'),
     api_secret=os.environ.get('CLOUDINARY_SECRET_KEY')
 )
+
+django_on_heroku.settings(locals())
