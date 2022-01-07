6c84fb9c547f160766c99912e1870fc22a8d300f
hannahhall <hannahkhall13@gmail.com>
2022-01-07 17:21:09 -0600

    hide admin:

--- baking_project/urls.py
+++ baking_project/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import path
 from baking_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(os.environ.get('ADMIN_URL') + '/', admin.site.urls),
     path('', views.IndexView.as_view(), name='bake-list'),
     path('<int:pk>/', views.DetailView.as_view(), name='bake-detail'),
 ]
